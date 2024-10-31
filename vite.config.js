db7d2ded49a8b3dbaf0569f3e84f6fa408d9adc9
brx-nbif <blondeau.nbif@mail.ru>
2024-10-31 11:18:49 +0100

    chore: add buid dir

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "deploy", // Replace <repo-name> with the name of your GitHub repository
+  base: "deploy",
+  build: {
+    outDir: "dist",
+  },
 });
